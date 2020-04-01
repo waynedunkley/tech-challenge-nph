ebd0e5553e78b037b394eda3ade7c9be3496b588
Wayne Dunkley <wdunkley84@gmail.com>
2020-04-01 23:32:33 +1100

    handle onSubmit, improve propType checking, generate output from form on submit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FormBuilder from './components/FormBuilder'
 import data from './cms/formData'
 import './App.css'
 
 function App() {
+  const [output, setOutput] = useState({})
+  const handleSubmit = (form) => {
+    const { elements } = form
+    const output = {
+      name: elements['name'].value,
+      dob: elements['dob'].value,
+      gender: elements['gender'].value,
+      contact: elements['contact'].value,
+      consentRequired: elements['consent'].checked,
+      guardianName: elements['guardianName'].checked,
+      guardianContact: elements['guardianContact'].checked,
+    }
+    setOutput(output)
+  }
   return (
     <div className="app">
-      <FormBuilder data={data} />
+      <FormBuilder fields={data.fields} onSubmit={handleSubmit} />
+      { output && (
+        <div>
+          { Object.keys(output).map(field => output[field]) }
+        </div>
+      ) }
     </div>
   )
 }
--- src/components/FormBuilder/index.js
+++ src/components/FormBuilder/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Input from '../../components/FormElements/Input'
 import InputMulti from '../../components/FormElements/InputMulti'
 
@@ -9,19 +10,35 @@ const componentSelector = {
   checkbox: InputMulti,
 }
 
-const FormBuilder = ({ data }) => {
-  const fields = data.fields.map(field => {
+const FormBuilder = ({ fields, onSubmit }) => {
+  const formComponents = fields.map(field => {
     const Component = componentSelector[field.type]
     if (Component) {
       return <Component {...field} name={field.id} key={field.id} />
     }
     return null
   })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onSubmit(e.currentTarget)
+  }
   return (
-    <>
-      { fields }
-    </>
+    <form onSubmit={handleSubmit}>
+      { formComponents }
+      <button type='submit'>Submit</button>
+    </form>
   )
+}
+
+FormBuilder.propTypes = {
+  fields: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    label: PropTypes.string,
+    options: PropTypes.arrayOf(PropTypes.object),
+  })).isRequired,
+  onSubmit: PropTypes.func.isRequired,
 }
 
 export default FormBuilder
